75745c64968e2d532893c6c495b34eca354512dd
shenjiayi <jiayis2@uci.edu>
2015-02-02 22:13:22 -0800

    add the code

---
+++ tech/tech/MainActivity.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.Threading;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace tech
+{
+
+	[Activity (Label = "tech", MainLauncher = true)]
+	public class MainActivity : Activity
+	{
+
+		private Button BtnLogin;
+		private EditText TxtUsername;
+		private EditText TxtPassword;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			TxtUsername = FindViewById<EditText> (Resource.Id.InputUsername);
+			TxtPassword = FindViewById<EditText> (Resource.Id.InputPassword);
+			BtnLogin = FindViewById<Button> (Resource.Id.BtnLogin);
+			BtnLogin.Click += BtnLogin_Click; 
+
+		}
+
+		void BtnLogin_Click(object sender,EventArgs e)
+		{
+			//the account is valid
+			string username = TxtUsername.Text;
+			string password = TxtPassword.Text;
+			BtnLogin.Text = "Logging";
+			ActLIikeRequest (username, password);
+			//Thread thread = new Thread (ActLIikeRequest);
+			//thread.Start ();
+
+
+
+			//account is not valid, a small window will show up
+
+			//FragmentTransaction transaction = FragmentManager.BeginTransaction ();
+			//test simplepage = new test();
+			//simplepage.Show (transaction, "hello world");
+		}
+
+		private void ActLIikeRequest(string username, string password)
+		{
+			//Thread.Sleep (3000);
+			if (username == "tech") {
+				Intent intent = new Intent (this, typeof(client));
+				this.StartActivity (intent);
+			} else {
+				FragmentTransaction transaction = FragmentManager.BeginTransaction ();
+				test simplepage = new test();
+				simplepage.Show (transaction, "hello world");
+			}
+		}
+
+	}
+}
+
+
---
+++ tech/tech/client.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Support.V4.Widget;
+using Android.Support.V4.App;
+
+namespace tech
+{
+	[Activity (Label = "client")]			
+	public class client : Activity
+	{
+		DrawerLayout DrawerLayout;
+		List<string> LeftItem = new List<string> ();
+		ArrayAdapter LeftAdapter;
+		ListView LeftDrawer;
+
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			SetContentView (Resource.Layout.clients);
+			// Create your application here
+
+			DrawerLayout = FindViewById<DrawerLayout> (Resource.Id.main);
+			LeftDrawer = FindViewById<ListView> (Resource.Id.ToolBar);
+
+			LeftItem.Add ("project 1");
+			LeftItem.Add ("project 2");
+			LeftItem.Add ("project 3");
+			LeftItem.Add ("project 4");
+			LeftItem.Add ("projects");
+			LeftItem.Add ("clients");
+			LeftItem.Add ("people");
+
+
+			LeftAdapter = new ArrayAdapter (this, Android.Resource.Layout.SimpleListItem1, LeftItem);
+			LeftDrawer.Adapter = LeftAdapter;
+
+		}
+	}
+}
+
