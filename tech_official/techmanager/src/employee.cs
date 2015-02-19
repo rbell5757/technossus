7b979b58e4ff825eaaa6a5b34e81eaa22610eb20
James Louie <jamlouie12@gmail.com>
2015-02-18 19:00:58 -0800

    Added new Official project, still in working some kinks out related to imports

---
+++ tech_official/techmanager/src/employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NavigationDrawer
+{
+	public class employee
+	{
+		public long Id { get; set; }
+		public string photo{ get; set; }
+		public string name{ get; set; }
+		public string available{get;set;}
+
+		public employee(long id, string photo,string name,string available)
+		{
+			this.Id = id;
+			this.photo = photo;
+			this.name = name;
+			this.available = available;
+		}
+
+	}
+}
+
